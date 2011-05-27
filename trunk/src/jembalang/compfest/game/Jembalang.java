f677ca9f13de57f9b0a0411b613d99486c96fa38
najogie@gmail.com <najogie@gmail.com@03d3d5e6-c60b-dfe6-73c3-66e0a808734e>
2011-05-27 07:12:10 +0000

    splash screen

--- trunk/src/jembalang/compfest/game/Jembalang.java
+++ trunk/src/jembalang/compfest/game/Jembalang.java
@@ -5,15 +5,19 @@ import android.os.Bundle;
 
 public class Jembalang extends Activity {
 	private GameView gameView;
+	private SplashScreen splashScreen;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        splashScreen = new SplashScreen();
         gameView = new GameView(this);
         setContentView(gameView);
         gameView.setFocusable(true);
         gameView.setFocusableInTouchMode(true);
         gameView.start();
     }
+    
+    
 }
