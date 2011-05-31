0742b971c765828fa8346984cb8a5d85a360c09b
najogie@gmail.com <najogie@gmail.com@03d3d5e6-c60b-dfe6-73c3-66e0a808734e>
2011-05-31 04:21:56 +0000

    play button

--- src/jembalang/compfest/game/Jembalang.java
+++ src/jembalang/compfest/game/Jembalang.java
@@ -2,19 +2,29 @@ package jembalang.compfest.game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Jembalang extends Activity {
 	private GameThread gameView;
+	private Button button;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        gameView = new GameThread(this);
-        setContentView(gameView);
-        gameView.setFocusable(true);
-        gameView.setFocusableInTouchMode(true);
-        gameView.start();
+        setContentView(R.layout.main);
+        button = (Button)findViewById(R.id.newgame);
+        button.setOnClickListener( new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+		        gameView = new GameThread(Jembalang.this);
+		        setContentView(gameView);
+		        gameView.setFocusable(true);
+		        gameView.setFocusableInTouchMode(true);
+		        gameView.start();
+			}
+		});
     }
     
     
